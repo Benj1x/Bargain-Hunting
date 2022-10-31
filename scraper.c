0a9027f223e9f70c71acffc487f2ca4ea5993b2d
Benj1x <benjaminh2001@hotmail.dk>
2022-10-31 12:11:39 +0100

    Setup

---
+++ scraper.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "scraper.h"
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
---
+++ scraper.h
@@ -0,0 +1,8 @@
+//
+// Created by Benjamin Høj on 31-10-2022.
+//
+
+#ifndef BARGAIN_HUNTING_SCRAPER_H
+#define BARGAIN_HUNTING_SCRAPER_H
+
+#endif //BARGAIN_HUNTING_SCRAPER_H
