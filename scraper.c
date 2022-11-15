c6aa7926c2596b72cd7e18a53036df7d04abba1a
Kragemand <ux66at@student.aau.dk>
2022-11-15 17:35:03 +0100

    WIP store_choice

--- scraper.c
+++ scraper.c
@@ -1,7 +1,62 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+#include <unistd.h> //access()
 #include "scraper.h"
+
+void store_choice();
+
+
 int main() {
-    printf("Hello, I will sell your personal information to the highest bidder!\n");
-    //i like cock
+    printf("Hello, I will sell your personal information\n");
+
+    store_choice();
+
     return 0;
+}
+
+
+void store_choice() {
+    FILE *stores;
+    char store_id [5];
+    char store_name [25];
+
+    /*if (access(stores, F_OK) != 0) {
+        stores = fopen("stores.txt", "w+");
+    } 
+    else {
+        stores = fopen("stores.txt", "r+");
+    }*/
+
+
+    stores = fopen("C:\\Users\\rasmu\\Documents\\GitHub\\Bargain-Hunting\\stores.txt", "a+");
+    
+    if (stores == NULL) {
+        printf("File couldn't be opened. Creating new file\n");
+        fclose(stores);
+        stores = fopen("C:\\Users\\rasmu\\Documents\\GitHub\\Bargain-Hunting\\stores.txt", "a+");        
+    }
+
+
+    while (1) {
+        printf("Enter store name, break with 'q':");
+        scanf("%[^\n]", &store_name);
+        while (getchar() != '\n');
+        if (store_name[0] == 'q') {
+            break;
+        }
+        else fprintf(stores, "%s\n", store_name);
+
+    }  
+    fclose(stores);
+
+    stores = fopen("C:\\Users\\rasmu\\Documents\\GitHub\\Bargain-Hunting\\stores.txt", "r");
+    int i = 0;
+    while (!feof(stores)) {
+        ++i;
+        fscanf(stores, "%[^\n]", store_name);
+        printf("Store number %d: %s\n", i, store_name);
+    }
+    fclose(stores);
+
 }
--- scraper.h
+++ scraper.h
@@ -1,8 +1,6 @@
-//
-// Created by Benjamin Høj on 31-10-2022.
-//
-
 #ifndef BARGAIN_HUNTING_SCRAPER_H
 #define BARGAIN_HUNTING_SCRAPER_H
+
+
 
 #endif //BARGAIN_HUNTING_SCRAPER_H
