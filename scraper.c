7d744cfa3ef4d349afd95edde36c56288adc85f7
Benj1x <benjaminh2001@hotmail.dk>
2022-11-15 11:48:32 +0100

    uwu

--- scraper.c
+++ scraper.c
@@ -1,7 +1,86 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "external/curl/curl.h"
+
 #include "scraper.h"
+
+typedef struct {
+    char* name;
+    char* description;
+    double price;
+    char* store;
+}product;
+
+
+int cmpfunc(const void* a, const void* b) {
+
+    double priceA = ((product*)a)->price;
+    double priceB = ((product*)b)->price;
+    if (priceA > priceB)
+        return 1;
+    if (priceB < priceB)
+        return -1;
+    else
+        return 0;
+
+}
+
+void sortByPrice(product* productList, int listLen) {
+    qsort(productList, listLen, sizeof(product), cmpfunc);
+}
+
+
+>>>>>>> Stashed changes
 int main() {
     printf("Hello, Joe!\n");
+    char* aifa = "hehea";
+    GetSallingProducts(aifa);
+    GetCoopProducts(aifa, aifa);
     return 0;
 }
 
+void GetSallingProducts(char* Item)
+{
+    printf("Hello, Salling!\n");
+
+
+}
+
+void GetCoopProducts(char* Item, char* Stores)
+{
+    char* StoreNumbers;
+    GetKardexNumbers(Stores, StoreNumbers);
+
+
+
+    printf("Hello, Coop!\n");
+}
+
+void GetKardexNumbers(char* Stores, char* KardexNumbers)
+{
+    CURL *curl = curl_easy_init();
+    curl_easy_setopt(curl, CURLOPT_URL,
+                     "https://www.googleapis.com/tasks/v1/users?name=pradeep&lastname=singla");
+    CURLcode curl_easy_getinfo(CURL *curl, CURLINFO info, ... );
+
+
+    curl_easy_perform(curl)
+    POST https://api.cl.coop.dk/storeapi/v1/stores?RetailGroupFilter=Fakta HTTP/1.1
+    Host: api.cl.coop.dk
+    Content-Type: application/json
+    Ocp-Apim-Subscription-Key: fefba58d42c4456ca7182cc307574653
+
+    {
+        Paging: { Size: 10, Page: 1 },
+        Querystring: "",
+            Filters:
+        [
+        {
+            Type: "RetailGroup",
+                RetailGroups: [ "fakta" ]
+        }
+        ]
+    }
+
+
+}
--- scraper.h
+++ scraper.h
@@ -2,7 +2,16 @@
 // Created by Benjamin Høj on 31-10-2022.
 //
 
+
+
+
+void GetSallingProducts(char* Item);
+void GetCoopProducts(char* Item, char* Stores);
+void GetKardexNumbers(char* Stores, char* KardexNumbers);
 #ifndef BARGAIN_HUNTING_SCRAPER_H
 #define BARGAIN_HUNTING_SCRAPER_H
 
 #endif //BARGAIN_HUNTING_SCRAPER_H
+
+add_executable (curl-demo curl.c)
+include_directories(${external})
