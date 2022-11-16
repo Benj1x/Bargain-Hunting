f0e41c8fd7254e466441603f4cb36c2aa22848e3
PG2W <akuzni22@student.aau.dk>
2022-11-16 09:49:10 +0100

    commitment

--- scraper.c
+++ scraper.c
@@ -2,6 +2,7 @@
 #include "scraper.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include "include/curl/curl.h"
 
 typedef struct {
     char* name;
@@ -28,6 +29,20 @@ void sortByPrice(product* productList, int listLen) {
     qsort(productList, listLen, sizeof(product), cmpfunc);
 }
 
+
+product* getRemaProducts(char* query) {
+    CURL* curl;
+    CURLcode res;
+    curl_global_init(CURL_GLOBAL_ALL);
+
+    curl = curl_easy_init();
+
+    if (curl) {
+        curl_easy_setopt(curl, CURLOPT_URL, "https://flwdn2189e-dsn.algolia.net/1/indexes/*/queries?x-algolia-agent=Algolia%%20for%%20vanilla%%20JavaScript%%203.21.1&x-algolia-application-id=FLWDN2189E&x-algolia-api-key=fa20981a63df668e871a87a8fbd0caed");
+        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, "{\"requests\":[{\"indexName\":\"aws-prod-products\",\"params\":\"query=&hitsPerPage=5000&facets=%%5B%%22labels%%22%%5D&filters=&page=0\"}]}");
+
+    }
+}
 
 int main() {
     printf("Hello, Joe!\n");
