94044c7c1ebd7465373df8751a8a385b8981139b
Benj1x <benjaminh2001@hotmail.dk>
2022-11-22 11:14:50 +0100

    s

--- scraper.c
+++ scraper.c
@@ -260,6 +260,76 @@ product* GetRemaProducts(char query[])
 
 }
 
+void storeChoice()
+{
+    FILE *stores;
+    char storeID [5];
+    char storeName [25];
+
+    stores = fopen("./stores.txt", "a+");
+
+    //Loop for entering wanted stores
+    while (1) {
+        printf("Enter store name, break with 'q':");
+        scanf("%[^\n]", &storeName);
+        while (getchar() != '\n');
+
+        //Check if entered store already is in list
+        if (storeCheck(storeName) == 1) {
+            printf("\nSame store is already in list\n");
+        }
+            //Breaks loop
+        else if (storeName[0] == 'q') {
+            break;
+        }
+            //Prints given store to list
+        else {
+            fprintf(stores, "\n%s", storeName);
+            printf("Given store is added to list\n\n");
+        }
+    }
+    fclose(stores);
+
+    //Prints stores present in list file
+    stores = fopen("./stores.txt", "r");
+    int i = 0;
+
+    while (!feof(stores)) {
+        ++i;
+        fgets(storeName, "%[^\n]", stores);
+        printf("Store number %d: %s\n", i, storeName);
+    }
+    fclose(stores);
+}
+
+//Function for checking if input store is already in list
+int storeCheck(char curretInput[])
+{
+    FILE *stores;
+    int storeExists = 0;
+    char searchString[20];
+    strcpy(searchString, curretInput);
+
+    stores = fopen("./stores.txt", "r");
+
+    char buffer[20];
+
+    while(fgets(buffer, 20, stores)) {
+        char *checkForStore = strstr(buffer, searchString);
+        if(checkForStore != NULL) {
+            storeExists = 1;
+            break;
+        }
+        else {
+            storeExists = 0;
+        }
+    }
+    fclose(stores);
+
+    //Returns 1 if store is found in list
+    //Returns 0 if store is not found in list
+    return storeExists;
+}
 
 char* APICall(APIStruct Params)
 {
@@ -369,7 +439,7 @@ int main()
     char query[6] = "kål";
 
     GetRemaProducts(query);
-
+    storeChoice();
     char name[30];
     double max_price;
     scan_input(name, &max_price);
--- scraper.h
+++ scraper.h
@@ -20,6 +20,8 @@ typedef struct string {
 char* GetSallingProducts(char* Item);
 char* GetCoopProducts(char* Item, char* Stores);
 void WriteAPIDataToFile(char* Items);
+void storeChoice();
+int storeCheck(char currentInput[]);
 char* APICall(APIStruct params);
 void init_string(struct string* s);
 size_t writefunc(void* ptr, size_t size, size_t nmemb, struct string* s);
